fd5587ded934cc715b4a3b19dcc79bb3eb31035c
Alan Fernandez <alan.f@msn.com>
2020-08-13 13:55:51 +0200

    Updated shop data with fixed IDs. Updated card utils to group items in cart reducer

--- src/components/collection-preview/collection-preview.component.js
+++ src/components/collection-preview/collection-preview.component.js
@@ -3,25 +3,20 @@ import './collection-preview.styles.scss';
 import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => {
-    
-    const renderItems = () =>{
-        return items
-        .filter((item, index) => index < 4 )
-        .map(({ id, ...otherItemProps}) => {
-            return (
-                <CollectionItem key={id} {...otherItemProps}> </CollectionItem>
-            );
-        })
-    }
-    
     return (
         <div className="collection-preview">
             <h1 className="title">{title.toUpperCase()}</h1>
             <div className="preview">
-                {renderItems()}
+            {items
+                .filter((item, index) => index < 4 )
+                .map((item) => {
+                return (
+                    <CollectionItem key={item.id} item={item} />
+                )})
+            }
             </div>
         </div>
     );
-};
+}; 
 
 export default CollectionPreview;
